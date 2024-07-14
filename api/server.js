5a907457ec957017a5c7f81507bfe08f5d88e39a
Mateo Lozano <119082981+MateoLoz@users.noreply.github.com>
2024-07-14 20:49:49 -0300

    Update server.js

--- api/server.js
+++ api/server.js
@@ -2,14 +2,11 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('users.json')
 const middlewares = jsonServer.defaults()
-
+const port = process.env.PORT || 3000
 server.use(middlewares)
 // Add this before server.use(router)
 
 server.use(router)
-server.listen(3000, () => {
-    console.log('JSON Server is running')
-})
-
+server.listen(port)
 // Export the Server API
 module.exports = server
