f0c7e774e7c280bbd0fe522b580bec2ebb734c35
Mateo Lozano <119082981+MateoLoz@users.noreply.github.com>
2024-07-14 21:19:37 -0300

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,6 +1,9 @@
 const jsonServer = require('json-server')
+
+const fs = require('fs')
+const db = JSON.parse(fs.readFileSync(path.join(__dirname, 'users.json')))
+const router = jsonServer.router(db)
 const server = jsonServer.create()
-const router = jsonServer.router('users.json')
 const middlewares = jsonServer.defaults()
 const port = process.env.PORT || 3000
 server.use(middlewares)
